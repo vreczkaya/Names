a0b1b8cb37ebe75dcf034247718fa834f1e5f8b3
Varvara Retskaya <vreckaa@gmail.com>
2023-09-06 02:03:00 +0300

    first commit

---
+++ HeatmapTask.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Names
+{
+    internal static class HeatmapTask
+    {
+        public static HeatmapData GetBirthsPerDateHeatmap(NameData[] names)
+        {
+            var dates = new string[30];
+            var months = new string[12];
+            for (int i = 0; i < dates.Length; i++)
+            {
+                dates[i] = (i + 2).ToString();
+            }
+            for (int i = 0; i < months.Length; i++)
+            {
+                months[i] = (i + 1).ToString();
+            }
+            var heatmap = new double[30, 12];
+            foreach (var name in names)
+            {
+                if (name.BirthDate.Day != 1) heatmap[name.BirthDate.Day - 2, name.BirthDate.Month - 1]++;
+            }
+            return new HeatmapData(
+                "Пример карты интенсивностей",
+                heatmap, 
+                dates, 
+                months);
+        }
+    }
+}
---
+++ HistogramTask.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Names
+{
+    internal static class HistogramTask
+    {
+        public static HistogramData GetBirthsPerDayHistogram(NameData[] names, string name)
+        {
+            var dates = new string[31];
+            for (int i = 0; i < dates.Length; i++)
+            {
+                dates[i] = (i + 1).ToString();
+            }
+            var birthCounts = new double[dates.Length];
+            for (int i = 0; i < names.Length; i++)
+            {
+                if (names[i].Name == name)
+                {
+                    birthCounts[names[i].BirthDate.Day - 1]++;
+                }
+            }
+            birthCounts[0] = 0;
+            return new HistogramData(
+                string.Format("Рождаемость людей с именем '{0}'", name),
+                dates,
+                birthCounts);
+        }
+    }
+}
